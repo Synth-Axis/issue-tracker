b510c8d5632b139ae0e2dc23b770a59ebf93736f
Synth-Axis <synth-etiks@hotmail.com>
2025-07-25 17:13:45 +0100

    route.ts issues corrected

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -8,13 +8,9 @@ export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = createIssueSchema.safeParse(body);
 
-  if (!validation.success)
-    return (
-      NextResponse.json(validation.error.format()),
-      {
-        status: 400,
-      }
-    );
+  if (!validation.success) {
+    return NextResponse.json(validation.error.format(), { status: 400 });
+  }
 
   const newIssue = await prisma.issue.create({
     data: {
