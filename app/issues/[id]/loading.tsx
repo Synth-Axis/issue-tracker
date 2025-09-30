c79bf148d94950551d8bffb1852ca235dcb063b9
Alexandre Garcia <alexandregarcia@Macmini.lan>
2025-09-30 14:25:19 +0100

    Add aditional loading skeletons

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Flex, Card, Box } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingIssueDetailPage = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box>
+      <Skeleton />
+      <Flex gap={"3"} my={"2"}>
+        <Skeleton width={50} />
+        <Skeleton width={100} />
+      </Flex>
+      <Card className=" prose mt-4">
+        <Skeleton count={3} />
+      </Card>
+    </Box>
+  );
 };
 
 export default LoadingIssueDetailPage;
