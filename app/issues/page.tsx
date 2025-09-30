d65cad671d8c934860607cf1908730c9df21701c
Alexandre Garcia <alexandregarcia@Macmini.lan>
2025-09-30 11:10:43 +0100

    Add loading skeletons

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -2,8 +2,8 @@
 
 import { useEffect, useState } from "react";
 import { Button, Table } from "@radix-ui/themes";
-import Link from "next/link";
 import IssueStatusBadge from "../components/IssueStatusBadge";
+import IssueActions from "./IssueActions";
 
 interface Issue {
   id: number;
@@ -38,11 +38,7 @@ const IssuesPage = () => {
 
   return (
     <div>
-      <div className="mb-5">
-        <Button>
-          <Link href="/issues/new">New Issue</Link>
-        </Button>
-      </div>
+      <IssueActions />
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
