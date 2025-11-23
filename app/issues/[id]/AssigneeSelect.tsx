bf212eb7de3dbd902c495f03eb55ce4ce445d7c8
Synth-Axis <synth-etiks@hotmail.com>
2025-11-23 10:36:46 +0000

    assign and issue to a user

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { User } from "@prisma/client";
+import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Skeleton } from "@/app/components";
 
-const AssigneeSelect = () => {
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const {
     data: users,
     error,
@@ -24,11 +24,24 @@ const AssigneeSelect = () => {
   if (error) return null;
 
   return (
-    <Select.Root>
+    <Select.Root
+      defaultValue={issue.assignedToUserId || "-"}
+      onValueChange={(userId) => {
+        const updatedUserId = userId === "-" ? null : userId; // Explicitly set to null if "-" is selected
+        axios
+          .patch(`/api/issues/${issue.id}`, {
+            assignedToUserId: updatedUserId,
+          })
+          .catch((err) => {
+            console.error("Error updating issue:", err); // Log any errors to the console
+          });
+      }}
+    >
       <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Sugestions</Select.Label>
+          <Select.Item value="-">Unasigned</Select.Item>
           {users?.map((user) => (
             <Select.Item key={user.id} value={user.id}>
               {user.name}
