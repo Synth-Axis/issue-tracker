71e0ac72c7acd5d5079ca0815e9b5b1149247bcf
Synth-Axis <synth-etiks@hotmail.com>
2025-11-29 16:33:52 +0000

    make columns sortable

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -3,8 +3,10 @@
 import { useEffect, useState } from "react";
 import { Table } from "@radix-ui/themes";
 import { Link, IssueStatusBadge } from "../../components";
+import NextLink from "next/link";
 import IssueActions from "./IssueActions";
 import { IssueStatus } from "@prisma/client";
+import { ArrowUpIcon } from "@radix-ui/react-icons";
 
 interface Issue {
   id: number;
@@ -14,14 +16,23 @@ interface Issue {
 }
 
 interface Props {
-  searchParams: { status: IssueStatus };
+  searchParams: {
+    status?: IssueStatus;
+    orderBy?: keyof Issue;
+    order?: "asc" | "desc";
+  };
 }
 
 const IssuesPage = ({ searchParams }: Props) => {
-  const statuses = Object.values(IssueStatus);
-  const status = statuses.includes(searchParams.status)
-    ? searchParams.status
-    : undefined;
+  const columns: {
+    label: string;
+    value: keyof Issue;
+    className?: string;
+  }[] = [
+    { label: "Issue", value: "title" },
+    { label: "Status", value: "status", className: "hidden md:table-cell" },
+    { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
+  ];
 
   const [issues, setIssues] = useState<Issue[]>([]);
 
@@ -49,29 +60,57 @@ const IssuesPage = ({ searchParams }: Props) => {
   return (
     <div>
       <IssueActions />
+
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Status
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Created
-            </Table.ColumnHeaderCell>
+            {columns.map((column) => {
+              const isActive = column.value === searchParams.orderBy;
+
+              const nextOrder =
+                isActive && searchParams.order === "asc" ? "desc" : "asc";
+
+              return (
+                <Table.ColumnHeaderCell key={column.value}>
+                  <NextLink
+                    href={{
+                      query: {
+                        ...searchParams,
+                        orderBy: column.value,
+                        order: nextOrder,
+                      },
+                    }}
+                  >
+                    {column.label}
+                  </NextLink>
+
+                  {isActive && (
+                    <>
+                      {searchParams.order === "asc" ? (
+                        <ArrowUpIcon className="inline-block ml-1" />
+                      ) : (
+                        <ArrowUpIcon className="inline-block ml-1 rotate-180" />
+                      )}
+                    </>
+                  )}
+                </Table.ColumnHeaderCell>
+              );
+            })}
           </Table.Row>
         </Table.Header>
+
         <Table.Body>
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
                 <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
-
                 <div className="block md:hidden"></div>
               </Table.Cell>
+
               <Table.Cell className="hidden md:table-cell">
                 <IssueStatusBadge status={issue.status} />
               </Table.Cell>
+
               <Table.Cell className="hidden md:table-cell">
                 {new Date(issue.createdAt).toLocaleDateString()}
               </Table.Cell>
@@ -84,6 +123,4 @@ const IssuesPage = ({ searchParams }: Props) => {
 };
 
 export const dynamic = "force-dynamic";
-//export const revalidate = 0;
-
 export default IssuesPage;
