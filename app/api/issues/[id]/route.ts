a046b71299a4ac690c7e942cccef771dd01cc7e4
Synth-Axis <synth-etiks@hotmail.com>
2025-11-30 20:17:40 +0000

    prisma changes

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -2,55 +2,46 @@ import authOptions from "@/app/auth/authOptions";
 import { patchIssueSchema } from "@/app/validationSchema";
 import { prisma } from "@/prisma/client";
 import { getServerSession } from "next-auth";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function PATCH(
-  request: NextRequest,
-  { params }: { params: { id: string } }
+  request: Request,
+  context: { params: { id: string } }
 ) {
-  const session = await getServerSession(authOptions);
+  const { params } = context;
 
-  if (!session) {
+  const session = await getServerSession(authOptions);
+  if (!session)
     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-  }
+
   const body = await request.json();
   const validation = patchIssueSchema.safeParse(body);
-  if (!validation.success) {
+
+  if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
-  }
 
   const { assignedToUserId, title, description } = body;
+
   if (assignedToUserId) {
     const user = await prisma.user.findUnique({
-      where: {
-        id: assignedToUserId,
-      },
+      where: { id: assignedToUserId },
     });
-    if (!user) {
+    if (!user)
       return NextResponse.json({ error: "Invalid user" }, { status: 400 });
-    }
   }
 
   const issue = await prisma.issue.findUnique({
-    where: {
-      id: parseInt(params.id),
-    },
+    where: { id: parseInt(params.id, 10) },
   });
 
-  if (!issue) {
+  if (!issue)
     return NextResponse.json({ message: "Invalid issue" }, { status: 404 });
-  }
 
   const updatedIssue = await prisma.issue.update({
-    where: {
-      id: issue.id,
-    },
-    data: {
-      title,
-      description,
-      assignedToUserId,
-    },
+    where: { id: issue.id },
+    data: { title, description, assignedToUserId },
   });
+
   return NextResponse.json(updatedIssue, { status: 200 });
 }
 
